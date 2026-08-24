b2a5046344ad93ecc4ef185f2da17b1548d60ed8
Artem Goryukhalov <goryukhalov@gmail.com>
2026-08-24 13:51:10 +0300

    lesson 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,16 @@
-print("Превед, медвед!")
+a = 7
+str_1 = "Медвед"
+b = 8.34
+#print(a)
+#a = 10
+#print(a)
+
+#a=b=c=0
+#a = b = c = 0, 0, 0
+
+#a, b = 8, 10
+#a, b = b, a
+#print(a)
+#print(b)
+
+print(type(b))

da5aee71a9f7bf388b4c35eef5aa9156e9a3b1f7
Artem Goryukhalov <goryukhalov@gmail.com>
2026-08-24 20:31:32 +0300

    lesson 2

---
+++ lesson _2/lesson_2_2.py
@@ -0,0 +1,18 @@
+s1 = 'Петр'
+s2 = "Огурцов"
+s3 = "\nПетро\nИванович"
+
+s4 = s1 + " " + s2
+s5 = len(s3)
+
+#print(s1 in s3)
+a = "аб"
+b = "ав"
+
+#print("Петр" in s1)
+print( a < b )
+
+print(ord("а"))
+print(ord("б"))
+print(ord("в"))
+

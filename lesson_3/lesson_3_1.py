cfcd7f2c264d852ea360d2098d441d54a1d34f6c
Artem Goryukhalov <goryukhalov@gmail.com>
2026-08-27 12:14:50 +0300

    lesson 3

---
+++ lesson_3/lesson_3_1.py
@@ -0,0 +1,21 @@
+#a = "string"
+#print(35, a)
+#print(5, 546.4, "qwerty", a)
+
+a = 12
+b = 7
+c = 101
+
+#print(a, b, c, sep=" | ", end="\n")
+#print(a, b, c, sep=" | ", end="")
+
+#print(f"Результат умножения числа {a} на число {b} равен {a * b}") #f string
+
+# d = float(input())
+# print(type(d))
+# print(d)
+
+a = float(input("Введите длину прямоугольника:"))
+b = float(input("Введите ширину прямоугольника:"))
+
+print(f'Периметр: {2 * (a + b)}')
---
+++ lesson_3/lesson_3_2.py
@@ -0,0 +1,34 @@
+# # # # # # print(5>3)
+# # # # # # print(5>8)
+# # # # # a = 11
+# # # # # b = 12
+# # # # # result = a >= b
+# # # # # print(type(result))
+# # # #
+# # # # # print(5 == 7 - 2)
+# # # # a = "asd"
+# # # # b = "asda"
+# # # # print(a != b)
+# # #
+# # # # x = 30
+# # # # print(x % 2 == 0) #проверка на четность
+# # #
+# # # x = 27
+# # # print(x % 3 == 0) #проверка на кратность
+# #
+# # a = 1.6
+# # print(a >=1 and a <= 2)
+# #
+# name = "Тимофей"
+# print(("и" in name) or ("у" in name))
+
+# x = 5
+# print(not((x % 2 == 0) or (x % 3 == 0)))
+
+# f = 45
+# print(bool(1))
+# print(bool(0))
+# print(bool(-50))
+# print(bool("dfg"))
+# print(bool(""))
+# print(bool(f))

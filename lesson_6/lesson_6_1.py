0bf8352a07698fa06f463069863d76b35aef92df
Artem Goryukhalov <goryukhalov@gmail.com>
2026-09-07 21:02:15 +0300

    lesson 6

---
+++ lesson_6/lesson_6_1.py
@@ -0,0 +1,39 @@
+# x = -6
+#
+# if x < 0:
+#     x = -x
+#
+# print(x)
+
+#
+# a = int(input("Введите число: "))
+# b = int(input("Введите число: "))
+# print("До IF:", a, b)
+# if a < b:
+#     a, b = b, a
+#
+# print("После IF", a, b)
+
+# x = 0
+# if x < -5 or x !=0:
+#     print("Число входит в диапазон")
+
+# ages = [52, 46,89,19]
+# age1 = 46
+# age2 = 48
+#
+# if age2 in ages:
+#     print ("Yes")
+# else:
+#     print ("No")
+
+x = int(input("Введите число: "))
+# if x < 0:
+#     print("Отрицательное число")
+# else:
+#     print("Положительное число")
+
+if x % 2 == 0:
+    print("четное число")
+else:
+    print("нечетное число")
---
+++ lesson_6/lesson_6_3.py
@@ -0,0 +1,12 @@
+a = 15
+b = 15
+
+if a > b:
+    res = a
+else:
+    res = b
+
+print(res)
+
+res = a if a > b else b
+print(res)
